8e81994a8009c33d46163372e5768cb5c248c5d3
Your Name <you@example.com>
2026-01-17 21:22:01 +0100

    Fixed import

--- app/src/main/java/org/unchained/rsvp_fast_read/ui/LibraryScreen.kt
+++ app/src/main/java/org/unchained/rsvp_fast_read/ui/LibraryScreen.kt
@@ -33,7 +33,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
